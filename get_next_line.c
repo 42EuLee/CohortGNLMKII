5d60be9ccecd02b9a1b663999beb27a448fa1cc6
VS-leu-lee <leu-lee@DESKTOP-8QSKJ43.localdomain>
2021-07-14 22:29:30 +0800

    Why change kill myself soon

---
+++ get_next_line.c
@@ -0,0 +1,78 @@
+#include "get_next_line.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < len)
+	{
+		((unsigned char *)b)[index] = c;
+		 index++;
+	}
+	return (b);
+}
+
+char	*ft_calloc(size_t count, size_t size)
+{
+	void	*heap;
+	size_t	total_size;
+
+	total_size = count * size;
+	heap = (char *)malloc(total_size);
+	if (!heap)
+		return (NULL);
+	ft_memset(heap, 0, total_size);
+	return (heap);
+}
+
+char	*get_good(char **str, char **line)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while ((*str)[i] && (*str)[i] != '\n')
+		i++;
+	if ((*str)[i] == '\n')
+	{
+		*line = ft_substr(*str, 0, i);
+		temp = ft_strdup(&((*str)[i + 1]));
+		free(*str);
+		*str = temp;
+	}
+	else
+	{
+		*line = ft_strdup(*str);
+		free(*str);
+		*str = NULL;
+		return (NULL);
+	}
+	return (*line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*line;
+	int			reader;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*temp;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	reader = 1;
+	while (!(ft_strchr(str, '\n')) && reader > 0)
+	{
+		reader = read(fd, buffer, BUFFER_SIZE);
+		if (reader < 0)
+			return (NULL);
+		if (!str)
+			str = ft_calloc(1, 1);
+		buffer[reader] = '\0';
+		temp = ft_strjoin(str, buffer);
+		free(str);
+		str = temp;
+	}
+	return (get_good(&str, &line));
+}
---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *str);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+
+#endif
